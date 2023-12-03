0bd09fa05035a5fc4c030a73a7e2149eecc6a160
Nii Derry <derricktagoe@gmail.com>
2023-12-03 10:32:05 +0000

    added section animation

--- script.js
+++ script.js
@@ -145,13 +145,56 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 // });
 
 // STICKY NAVIGATION: INTERSECTION OBSERVER API
-const obsCallback = function (entries, observer) {};
-const ObsOptions = {
-  root: null,
-  threshold: 0.1,
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   });
+// };
+// const ObsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+// const observer = new IntersectionObserver(obsCallback, ObsOptions);
+// observer.observe(section1);
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
 };
-const observer = new IntersectionObserver(obsCallback, ObsOptions);
-observer.observe(section1);
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+// REVEAL SECTIONS
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 
 /*
 
